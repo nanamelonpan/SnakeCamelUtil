7476b94747ef7673e9ea875b80b5b34266c0e695
g16923kn <g16923kn@gm.tsuda.ac.jp>
2018-05-08 17:46:17 +0900

    Testを追加

--- test/snakecamel/SnakeCamelTest.java
+++ test/snakecamel/SnakeCamelTest.java
@@ -1,14 +1,31 @@
 package snakecamel;
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
+
 public class SnakeCamelTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void スネークケースの文字列をキャメルケースに変換() {
+		SnakeCamelUtil scu= new SnakeCamelUtil();
+		String expected="AbcDefGhi";
+		String actual = scu.snakeToCamelcase("abc_def_ghi"); 
+		assertEquals(expected,actual);
+		
+	
+				
+
 	}
 
+	@Test
+	public void キャメルケースの文字列をスネークケースに変換する() {
+		SnakeCamelUtil scu= new SnakeCamelUtil();
+		String expected = "abc_def_ghi";
+		String actual = scu.camelToSnakecase("AbcDefGhi"); 
+		assertEquals(expected,actual);
+				
+
+	}
 }
